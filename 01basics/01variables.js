cc0abfc1a8fa7435017ab30159f9d068a1e2402f
saadzubair1 <168035682+saadzubair1@users.noreply.github.com>
2024-04-24 22:41:47 +0000

    datatypes

---
+++ 01basics/01variables.js
@@ -0,0 +1,12 @@
+const accId  = 498
+let accEmail = "saad@gmail.com" //recommended method
+var accPassword = "12345" // not recommended
+accCity = "Lahore" // not recommended
+
+// accId = 2
+accEmail = "changed@gmail.com"
+accPassword = "11111"
+accCity = "kiraanchi"
+
+// console.log(accId);
+console.table([accId, accEmail, accPassword, accCity])
---
+++ 01basics/02datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; // use all code in newer JS versions limits and restrictions
+
+let name = "saad"
+let age = 23
+let height = null
+
+console.log(typeof height);
